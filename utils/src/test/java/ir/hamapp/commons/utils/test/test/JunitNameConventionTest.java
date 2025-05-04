de7f907d6dc83025545f0ec90ad033d000f13640
H.Rashid <hamidreza.rashid@gmail.com>
2025-05-04 10:39:44 +0330

    bug fixes

--- utils/src/test/java/ir/hamapp/commons/utils/test/test/JunitNameConventionTest.java
+++ utils/src/test/java/ir/hamapp/commons/utils/test/test/JunitNameConventionTest.java
@@ -1,10 +1,7 @@
 package ir.hamapp.commons.utils.test.test;
 
-import ir.hamapp.commons.utils.test.JunitNameConvention;
-import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
 
-@DisplayNameGeneration(JunitNameConvention.class)
 class JunitNameConventionTest {
 
     @Test
